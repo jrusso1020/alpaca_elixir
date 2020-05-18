c0a2b5d7c929778b8952df92b864189e053dba51
jrusso1020 <jdrusso1020@gmail.com>
2020-05-17 21:33:52 -0700

    Allow to query for account and basic Tesla Client
    Add the initial account module with typed struct and
    get function. Move to tesla for client since I was having
    problems with HTTPoison and I like how Tesla is similar to
    faraday. Add initial tests as well and other configuration
    tooling and deps

---
+++ lib/alpaca/response_middleware.ex
@@ -0,0 +1,43 @@
+defmodule Alpaca.ResponseMiddleware do
+  @moduledoc """
+  This middleware handles responses from the Alpaca API and
+  returns it in a result tuple of either {:ok, reponse_body} or
+  {:error, error_message}.
+
+  This allows us to easily handle all of our responses in a uniform
+  way.
+
+
+  ### Example usage
+
+  ```
+  defmodule AlpavaClient do
+    use Tesla
+
+    plug Alpaca.ResponseMiddleware
+  end
+  ```
+
+  ### Options
+  There are no options at the current moment in time
+  """
+  @behaviour Tesla.Middleware
+
+  def call(env, next, _options) do
+    env
+    |> Tesla.run(next)
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, response}) do
+    case response.status do
+      status when status in 200..299 ->
+        {:ok, Jason.decode!(response.body, keys: :atoms)}
+
+      status when status in 400..599 ->
+        {:error, %{status: status, body: Jason.decode!(response.body, keys: :atoms)}}
+    end
+  end
+
+  defp handle_response(response), do: response
+end
--- test/alpaca_elixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AlpacaElixirTest do
-  use ExUnit.Case
-  doctest AlpacaElixir
-
-  test "greets the world" do
-    assert AlpacaElixir.hello() == :world
-  end
-end
