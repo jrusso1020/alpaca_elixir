5a6346cbea0230d7c34aec9d7126c1f71476f74b
jrusso1020 <jdrusso1020@gmail.com>
2020-06-02 17:03:30 -0700

    version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlpacaElixir.MixProject do
   def project do
     [
       app: :alpaca_elixir,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       preferred_cli_env: ["coveralls.html": :test],
       start_permanent: Mix.env() == :prod,
