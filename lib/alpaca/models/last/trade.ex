904aea9a8baa768fe65aa054ed978267dbeb62b9
jrusso1020 <jdrusso1020@gmail.com>
2020-06-20 14:54:24 -0400

    Last Trade Data API
    add last trade stock information data API endpoint

---
+++ lib/alpaca/models/last/trade.ex
@@ -0,0 +1,20 @@
+defmodule Alpaca.Last.Trade do
+  @moduledoc """
+  A resource that allows us to query for the last trade of a symbol
+
+  An last trade activity has the following methods we can call on it
+  ```
+  get/1
+  ```
+
+  The `get/1` method allows us to get a singulat last trade for a given symbol by calling
+  `Alpaca.Last.Trade.get(symbol)`. Where `symbol` is the
+  stock symbol you want to retrieve last trade information for.
+  """
+  alias Alpaca.Client
+
+  use Alpaca.Resource,
+    endpoint: "last/stocks",
+    exclude: [:list, :create, :edit, :delete, :delete_all, :update],
+    opts: [version: "v1", api_host: Client.data_api_host()]
+end
---
+++ test/alpaca/models/last/trade_test.exs
@@ -0,0 +1,21 @@
+defmodule Alpaca.TradeTest do
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  alias Alpaca.Last.Trade
+
+  describe "get/1" do
+    test "gets the last trade successfully" do
+      use_cassette "last_trade/get_success" do
+        assert {:ok, %{status: "success", symbol: "AAPL", last: last}} = Trade.get("AAPL")
+      end
+    end
+
+    test "gets the last trade unsuccessfully" do
+      use_cassette "last_trade/get_failure" do
+        assert {:error, %{status: 404, body: %{code: 40_410_000, message: "resource not found"}}} =
+                 Trade.get("FFFFF")
+      end
+    end
+  end
+end
